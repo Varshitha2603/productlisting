c2fae470f325b5aab4c22489575befe184ebbdac
DCKAP <varshithaksdckap@gmail.com>
2024-01-13 14:19:29 +0530

    productlist

---
+++ product.js
@@ -0,0 +1,14 @@
+"use strict"
+
+function myFunction(back)
+{
+    back = document.querySelector("#anchor").href;
+}
+
+// let btn = document.querySelector("#btn1");
+// btn.addEventListener("click", ()=> {
+//     let anchor = document.createElement("a");
+//     anchor.setAttribute("href", "index.html");
+//     // console.log(anchor);
+//     btn.append(anchor);
+// });
